4438305412879c35cef787192d507ad850fbdd14
Suraj <contact@suninthesky.co.uk>
2016-08-09 16:43:33 +0100

    Initial middleman migration

--- config.rb
+++ config.rb
@@ -23,6 +23,8 @@ configure :development do
   activate :livereload
 end
 
+activate :directory_indexes
+
 ###
 # Helpers
 ###
@@ -37,8 +39,10 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  activate :asset_hash
 end
